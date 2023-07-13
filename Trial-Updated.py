6ad6bc91d94972b43843581e40fdaffc0cb66141
DT1717 <137271304+DT1717@users.noreply.github.com>
2023-07-13 12:38:03 +0200

    Trial-Updated.py

--- Trial-Updated.py
+++ Trial-Updated.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+from datetime import datetime
+import pytz
 
 def perform_task(df, task):
     if task == 'Show First Rows':
@@ -23,18 +25,21 @@ def perform_task(df, task):
         st.write('Cleaned Data:')
         st.write(df)
     elif task in ['Plot Bar Graph', 'Plot Line Graph']:
-        columns_to_select = st.multiselect("Choose two columns", df.columns, default=df.columns[:2])
+        columns_to_select = st.sidebar.multiselect("Choose two columns", df.columns, default=df.columns[:2])
+        plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
+        plot_width = st.sidebar.slider('Specify plot width', 200, 500, 250)
         if len(columns_to_select) != 2:
             st.warning("Please select exactly two columns.")
         else:
             kind = 'bar' if task == 'Plot Bar Graph' else 'line'
+            plt.figure(figsize=(plot_width/100, plot_height/100))  # Adjusting the size of the plot using the sliders
             df[columns_to_select].plot(kind=kind, x=columns_to_select[0], y=columns_to_select[1])
             plt.xlabel(columns_to_select[0])
             plt.ylabel(columns_to_select[1])
             plt.title(f"{kind.capitalize()} Graph for {columns_to_select[0]} vs {columns_to_select[1]}")
             st.pyplot(plt)
     elif task == 'Compare Two Columns':
-        columns_to_select = st.multiselect("Choose two columns for comparison", df.columns, default=df.columns[:2])
+        columns_to_select = st.sidebar.multiselect("Choose two columns for comparison", df.columns, default=df.columns[:2])
         if len(columns_to_select) != 2:
             st.warning("Please select exactly two columns for comparison.")
         else:
@@ -45,6 +50,17 @@ def perform_task(df, task):
         st.error(f"Command '{task}' not recognized.")
 
 def main():
+    st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        
+    st.sidebar.header('Dashboard `version 2`')
+    st.sidebar.markdown('Text description goes here')
+
+    st.sidebar.subheader('Line/Bar chart parameters')
+    plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
+
     st.title("Data Analysis App")
     st.markdown("""
     This Web App is a tool for carrying out fundamental data analysis operations on a CSV or Excel file, it is meant to speed up the process.
@@ -74,6 +90,12 @@ def main():
         manual_task = st.text_input("")
         if manual_task:
             perform_task(df, manual_task)
+
+    # Display current date and time in Norway timezone
+    oslo = pytz.timezone('Europe/Oslo')
+    oslo_time = datetime.now(oslo)
+    st.sidebar.markdown("### Date and Time")
+    st.sidebar.text(f"Oslo, Norway: {oslo_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 if __name__ == "__main__":
     main()

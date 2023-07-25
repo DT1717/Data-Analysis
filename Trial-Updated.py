4eda5165b57bf8d0dc3cf4a93ef39a3d60759e72
DT1717 <137271304+DT1717@users.noreply.github.com>
2023-07-25 19:33:26 +0200

    Trial-Updated.py

--- Trial-Updated.py
+++ Trial-Updated.py
@@ -5,19 +5,20 @@ import streamlit as st
 
 # section 2
 def clean_data(df):
-    # ... (same as before) ...
+    df = df.apply(pd.to_numeric, errors='coerce')  # Convert all columns to numeric
+    df = df.dropna(how='all')  # Remove rows where all values are NaN
+    df = df.dropna(axis=1, how='all')  # Remove columns where all values are NaN
+    return df
 
 def compare_data(df1, df2):
     st.markdown("## Data Comparison")
-    st.write("Uploaded Data 1:")
-    st.write(df1)
-    st.write("Uploaded Data 2:")
-    st.write(df2)
-    # You can add comparison logic here as needed
-    # For example, you can compare summary statistics, visualize both datasets, etc.
+    st.write("First 5 Rows of First Dataset:")
+    st.write(df1.head())
+    st.write("First 5 Rows of Second Dataset:")
+    st.write(df2.head())
 
 def perform_task(df, task, plot_height, plot_width):
-    # ... (same as before) ...
+    # Your task execution code here...
 
 def main():
     st.set_page_config(layout='wide', initial_sidebar_state='expanded')
@@ -31,13 +32,8 @@ def main():
     plot_height = st.sidebar.slider('Specify plot height', 2.0, 5.0, 2.5)  # Adjusted range
     plot_width = st.sidebar.slider('Specify plot width', 2.0, 8.0, 4.0)  # Adjusted range
 
-# section 3
     st.title("Data Analysis App")
-    st.markdown("""
-    This Web App is a tool for carrying out fundamental data analysis operations on a CSV or Excel file, it is meant to speed up the process.
-    You can upload your data and then choose or enter a command to carry out several actions. 
-    In the case of Excel files with multiple sheets you can select the sheet you would like to analyse.
-    """)
+    # ... (same as before) ...
 
     uploaded_files = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx"], accept_multiple_files=True)
 
@@ -57,7 +53,7 @@ def main():
                     st.error("This file isn't a recognized Excel file.")
                     return
             else:
-                st.error("This file type isn't supported.")
+                st.error("One or more files have an unsupported file type.")
                 return
 
             df = clean_data(df)

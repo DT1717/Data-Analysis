3b7e7bdb735a297ddcc4c6701d01859c9324ce7b
DT1717 <137271304+DT1717@users.noreply.github.com>
2023-07-25 12:58:58 +0200

    Trial-Updated.py

--- Trial-Updated.py
+++ Trial-Updated.py
@@ -1,4 +1,4 @@
-# section 1 
+# section 1
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
@@ -11,55 +11,25 @@ def clean_data(df):
     return df
 
 def perform_task(df, task, plot_height, plot_width):
-    if task == 'Show First Rows':
-        st.write(df.head())
-    elif task == 'Show Last Rows':
-        st.write(df.tail())
-    elif task == 'Show Columns':
-        st.write(df.columns.to_list())
-    elif task == 'Show Dimensions':
-        st.write(df.shape)
-    elif task == 'Show Summary':
-        st.write(df.describe())
-    elif task == 'Show Missing Value Counts':
-        st.write(df.isna().sum())
-    elif task == 'Delete Rows With Missing Values':
-        missing_rows = df.loc[df.isna().any(axis=1), :]
-        st.write('Deleted Rows:')
-        st.write(missing_rows)
-        df.dropna(inplace=True)
-        st.write('Cleaned Data:')
-        st.write(df)
-    elif task in ['Plot Bar Graph', 'Plot Line Graph']:
-        if len(df.columns) >= 2:
-            default_columns = df.columns[:2].tolist()
-        else:
-            default_columns = df.columns.tolist()
-        columns_to_select = st.multiselect("Choose two columns", df.columns, default=default_columns)
-        if len(columns_to_select) != 2:
-            st.warning("Please select exactly two columns.")
-        else:
-            if pd.api.types.is_numeric_dtype(df[columns_to_select[0]]) and pd.api.types.is_numeric_dtype(df[columns_to_select[1]]):
-                kind = 'bar' if task == 'Plot Bar Graph' else 'line'
-                fig, ax = plt.subplots(figsize=(plot_width, plot_height))
-                df.plot(kind=kind, x=columns_to_select[0], y=columns_to_select[1], ax=ax)
-                plt.xlabel(columns_to_select[0])
-                plt.ylabel(columns_to_select[1])
-                plt.title(f"{kind.capitalize()} Graph for {columns_to_select[0]} vs {columns_to_select[1]}")
-                st.pyplot(fig)
-            else:
-                st.warning("Both selected columns must be numeric for plotting.")
-    elif task == 'Compare Two Columns':
-        columns_to_select = st.multiselect("Choose two columns for comparison", df.columns, default=df.columns[:2])
-        if len(columns_to_select) != 2:
-            st.warning("Please select exactly two columns for comparison.")
-        else:
-            fig, ax = plt.subplots(figsize=(plot_width, plot_height))
-            df.plot(kind='scatter', x=columns_to_select[0], y=columns_to_select[1], ax=ax)
-            plt.title(f"Comparison between {columns_to_select[0]} and {columns_to_select[1]}")
-            st.pyplot(fig)
-    else:
-        st.error(f"Command '{task}' not recognized.")
+    # ... (same as before) ...
+
+def compare_data(df1, df2):
+    st.markdown("## Data Comparison")
+    st.write("Uploaded Data 1:")
+    st.write(df1)
+    st.write("Uploaded Data 2:")
+    st.write(df2)
+
+    # Add comparison logic here as needed
+    # For example, you can compare summary statistics, visualize both datasets, etc.
+
+def save_data(df, file_name):
+    df.to_csv(file_name, index=False)
+    st.success(f"Data saved as {file_name}")
+
+def save_graph(fig, file_name):
+    fig.savefig(file_name)
+    st.success(f"Graph saved as {file_name}")
 
 def main():
     st.set_page_config(layout='wide', initial_sidebar_state='expanded')
@@ -73,53 +43,63 @@ def main():
     plot_height = st.sidebar.slider('Specify plot height', 2.0, 5.0, 2.5)  # Adjusted range
     plot_width = st.sidebar.slider('Specify plot width', 2.0, 8.0, 4.0)  # Adjusted range
 
-# section 3
     st.title("Data Analysis App")
-    st.markdown("""
-    This Web App is a tool for carrying out fundamental data analysis operations on a CSV or Excel file, it is meant to speed up the process.
-    You can upload your data and then choose or enter a command to carry out several actions. 
-    In the case of Excel files with multiple sheets you can select the sheet you would like to analyse.
-    """)
+    # ... (same as before) ...
 
-    uploaded_file = st.file_uploader("Upload CSV or Excel file", type=["csv", "xlsx"])
+    uploaded_files = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx"], accept_multiple_files=True)
 
-    if uploaded_file is not None:
-        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type, "FileSize": uploaded_file.size}
-        st.write(file_details)
-        
-        if "csv" in uploaded_file.type:
-            try:
-                df = pd.read_csv(uploaded_file)
-            except UnicodeDecodeError:
-                st.error("This CSV file isn't UTF-8 encoded.")
+    if uploaded_files:
+        data_frames = []
+        for uploaded_file in uploaded_files:
+            if "csv" in uploaded_file.type:
+                try:
+                    df = pd.read_csv(uploaded_file)
+                except UnicodeDecodeError:
+                    st.error("This CSV file isn't UTF-8 encoded.")
+                    return
+            elif "xlsx" in uploaded_file.type:
+                try:
+                    df = pd.read_excel(uploaded_file, engine='xlrd')
+                except ValueError:
+                    st.error("This file isn't a recognized Excel file.")
+                    return
+            else:
+                st.error("One or more files have an unsupported file type.")
                 return
-        elif "excel" in uploaded_file.type:
-            try:
-                df = pd.read_excel(uploaded_file)
-            except ValueError:
-                st.error("This file isn't a recognized Excel file.")
-                return
-        else:
-            st.error("This file type isn't supported.")
-            return
 
-# section 4
-        df = clean_data(df)
+            df = clean_data(df)
+            data_frames.append(df)
 
-        st.markdown("## Uploaded Data")
-        st.write(df)
+        if len(data_frames) > 1:
+            # Compare Data
+            compare_data(data_frames[0], data_frames[1])
+
+        st.markdown("## Cleaned Data")
+        st.write(data_frames[0])  # Displaying the first uploaded data
 
         tasks = ['Show First Rows', 'Show Last Rows', 'Show Columns', 'Show Dimensions', 'Show Summary',
                  'Show Missing Value Counts', 'Delete Rows With Missing Values', 'Plot Bar Graph',
                  'Plot Line Graph', 'Compare Two Columns']
         st.markdown("## Choose an Analysis Task")
         task = st.selectbox("", tasks)
-        perform_task(df, task, plot_height, plot_width)
+        perform_task(data_frames[0], task, plot_height, plot_width)
 
         st.markdown("## Enter a Custom Analysis Task")
         manual_task = st.text_input("")
         if manual_task:
-            perform_task(df, manual_task, plot_height, plot_width)
+            perform_task(data_frames[0], manual_task, plot_height, plot_width)
+
+        # Add the option to download cleaned data and graphs
+        if st.button("Download Cleaned Data"):
+            save_data(data_frames[0], "cleaned_data.csv")
+
+        if st.button("Download Graph"):
+            fig, ax = plt.subplots(figsize=(plot_width, plot_height))
+            data_frames[0].plot(kind='bar', x=data_frames[0].columns[0], y=data_frames[0].columns[1], ax=ax)
+            plt.xlabel(data_frames[0].columns[0])
+            plt.ylabel(data_frames[0].columns[1])
+            plt.title(f"Bar Graph for {data_frames[0].columns[0]} vs {data_frames[0].columns[1]}")
+            save_graph(fig, "graph.png")
 
 if __name__ == "__main__":
     main()
